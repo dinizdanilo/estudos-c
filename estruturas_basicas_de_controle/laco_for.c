559174576649a26297a95c806566f937e6948715
Danilo Diniz <dgdevbr@gmail.com>
2026-03-23 23:34:20 -0300

    laço for decrementando

--- estruturas_basicas_de_controle/laco_for.c
+++ estruturas_basicas_de_controle/laco_for.c
@@ -4,4 +4,14 @@ int main() {
     for(int i = 0; i < 5; i++) {
         printf("Numero: %d\n", i);
     }
+
+    printf("\nContagem regeressiva: \n");
+
+    for (int i = 10; i >= 1; i--) {
+        printf("%d...\n", i);
+    }
+
+    printf("## BOOM! ##\n");
+
+    return 0;
 }
