8d7608b48a36d3b8242227d2f93716aba7992954
Danilo Diniz <dgdevbr@gmail.com>
2026-04-04 22:38:38 -0300

    funcao tipo  void

---
+++ funcoes_e_bibliotecas/funcao_void.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void exibirMenu() {
+    printf("=== MENU DE OPÇÕES ===\n");
+    printf("1 - Ver saldo\n");
+    printf("2 - Fazer depósito\n");
+    printf("3 - Fazer saque\n");
+    printf("4 - Sair\n");
+    printf("======================\n");
+}
+
+int main() {
+    exibirMenu();
+
+    int opcao;
+    printf("Escolha uma opção: ");
+    scanf("%d%*c", &opcao);
+
+    printf("Você escolheu a opção %d\n", opcao);
+
+    return 0;
+}
