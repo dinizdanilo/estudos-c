ede25d00a450022538ecd1b5a75b864a1db4c912
Danilo Diniz <dgdevbr@gmail.com>
2026-04-11 23:03:22 -0300

    vetor de strings

---
+++ estruturas_de_dados_basicas/vetor_string.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    char nomes[3][20];
+
+    for (int i = 0; i < 3; i++) {
+        printf("Digite o nome %d : ", i + 1);
+        scanf("%s", nomes[i]);
+    }
+
+    for (int i = 0; i < 3; i++) {
+        printf("Nome %d: %s\n", i + 1, nomes[i]);
+    }
+
+    return 0;
+}
